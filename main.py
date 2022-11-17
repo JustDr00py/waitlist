3ee95e06648c1df96afc39ae9a13d0a4450d85d2
JustDr00py <dtuando@bns.email>
2022-11-16 17:41:44 -0800

    Cleaned and Updated

--- main.py
+++ main.py
@@ -3,91 +3,84 @@ from time import sleep
 from datetime import datetime
 from os.path import exists
 
-# list of name, degree, score
-nme = []
-sze = []
-tme = []
- # dictionary of lists
-data = {'Name': nme, 'Party Size': sze, 'Time': tme}
-options = ['new', 'update', 'remove', 'show']
-count = 0
+class MyData:
+    nme = []
+    sze = []
+    tme = []
+    # dictionary of lists
+    data = {'Name': nme, 'Party Size': sze, 'Time': tme}
+    options = ['new', 'update', 'remove', 'show']
+    count = 0
 
-df = pd.DataFrame(data) 
-def new_data():
-    global nme
-    global sze
-    global tme
-    global df
-    name = input('Name: ')
-    nme.append(name)
-    size = input('Size: ')    
-    sze.append(size)
-    now = datetime.now()
-    time = now.strftime("%I:%M:%S")
-    tme.append(time)
-    df = pd.DataFrame(data)
-    #print(df)
+    df = pd.DataFrame(data) 
+    def __init__(self):
+        pass
 
-def save_data():
-    global df
-    df.to_csv('waitlist.csv', index=False)
-    print('Saving Data...')
+    def new_data():
+        name = input('Name: ')
+        MyData.nme.append(name)
+        size = input('Size: ')    
+        MyData.sze.append(size)
+        now = datetime.now()
+        time = now.strftime("%I:%M:%S")
+        MyData.tme.append(time)
+        MyData.df = pd.DataFrame(MyData.data)
+        #print(df)
 
-def load_data():
-    print('Loading Data...')
-    global df
-    df = pd.read_csv('waitlist.csv')
-    for item in df['Name'].values.tolist():
-        #print(item)
-        nme.append(item)
-    for item in df['Party Size'].values.tolist():
-        #print(item)
-        sze.append(item)
-    for item in df['Time'].values.tolist():
-        #print(item)
-        tme.append(item)
-        
-def show():
-    global df
-    df = pd.read_csv('waitlist.csv')
-    print(df)
+    def save_data():
+        MyData.df.to_csv('waitlist.csv', index=False)
+        print('Saving Data...')
 
-def update(x):
-    global df
-    if x in df['Name'].values.tolist():
-        new_name = input('New Name: ')
-        df['Name'] = df['Name'].replace([x], new_name)
-        print(df)
-        df.to_csv('waitlist.csv', index=False)
-        print('Updated Name!')
-        
-def remove(x):
-    global df
-    if x in df['Name'].values.tolist():
-        df.drop(df.index[df['Name'] == x], inplace=True)
-        print(df)
-        df.to_csv('waitlist.csv', index=False)
-        print('Removed Name!')
+    def load_data():
+        print('Loading Data...')
+        MyData.df = pd.read_csv('waitlist.csv')
+        for item in MyData.df['Name'].values.tolist():
+            #print(item)
+            MyData.nme.append(item)
+        for item in MyData.df['Party Size'].values.tolist():
+            #print(item)
+            MyData.sze.append(item)
+        for item in MyData.df['Time'].values.tolist():
+            #print(item)
+            MyData.tme.append(item)
+            
+    def show():
+        MyData.df = pd.read_csv('waitlist.csv')
+        print(MyData.df)
+
+    def update(x):
+        if x in MyData.df['Name'].values.tolist():
+            new_name = input('New Name: ')
+            MyData.df['Name'] = MyData.df['Name'].replace([x], new_name)
+            print(MyData.df)
+            MyData.df.to_csv('waitlist.csv', index=False)
+            print('Updated Name!')
+            
+    def remove(x):
+        if x in MyData.df['Name'].values.tolist():
+            df.drop(df.index[MyData.df['Name'] == x], inplace=True)
+            print(MyData.df)
+            df.to_csv('waitlist.csv', index=False)
+            print('Removed Name!')
             
 #Initialize
 if exists('waitlist.csv'):
-    load_data()
+    MyData.load_data()
 else:
-    df = pd.DataFrame(data)
+    df = pd.DataFrame(MyData.data)
     df.to_csv('waitlist.csv')
 
 #Main Loop
 while True:
-    q = input(f'What would you like to do? \n {options}: \n')
+    q = input(f'What would you like to do? \n {MyData.options}: \n')
     if 'new' in q:
-        new_data()
-        save_data()
-        count+=1
+        MyData.new_data()
+        MyData.save_data()
     if 'update' in q:
         name = input('Who would you like to Update?: \n')
-        update(name)
+        MyData.update(name)
     if 'remove' in q:
         name = input('Who would you like to Remove?: \n')
-        remove(name)
+        MyData.remove(name)
     if 'show' in q:
-        show()
+        MyData.show()
